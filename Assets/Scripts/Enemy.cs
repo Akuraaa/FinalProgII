3fa2c75a18073a22a09e1e4cf800f42ce2846660
Akuraaa <72999443+Akuraaa@users.noreply.github.com>
2021-07-28 13:37:45 -0300

    Enemies Pack

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float minIdleTime = 3;
+    public float maxIdleTime = 5;
+    public float viewAngle = 90;
+    public bool stun;
+
+    protected Animator _anim;
+  //protected StateMachine _sm;
+    protected AudioSource _audio;
+  //protected PlayerController player;
+  //protected EnemyDecisionTree _enemyTree;
+    private void Awake()
+    {
+        //_sm = new StateMachine();
+        _anim = GetComponent<Animator>();
+        //player = FindObjectOfType<PlayerController>();
+        _audio = GetComponent<AudioSource>();
+        //_enemyyTree = 
+    }
+
+    private void Start()
+    {
+         
+    }
+}
